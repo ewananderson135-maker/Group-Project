ff28802fe3d67bb69078910ef6195f74d2cd1871
Michelle <mjian4@ocdsb.ca>
2026-05-08 11:33:27 -0400

    a

--- Item.java
+++ Item.java
@@ -7,27 +7,13 @@
  */
 public class Item
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Item
-     */
-    public Item()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    private short shrPoints;
+   private String strColour;
+   
+   Item(short p, String c)
+   {
+       this.shrPoints = p;
+       this.strColour = c;
+   }
+   
 }
--- SuperFood.java
+++ SuperFood.java
@@ -7,27 +7,9 @@
  */
 public class SuperFood extends Item
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class SuperFood
-     */
-    public SuperFood()
+   SuperFood(short p, String c)
     {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        super((short)10, "Green");
+        
     }
 }
