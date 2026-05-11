ba134742b323535ced233b161cbd0a6334ea965d
Nathaniel Benedik <nbene1@ocdsb.ca>
2026-05-11 09:26:35 -0400

    update

--- Player.java
+++ Player.java
@@ -8,5 +8,23 @@
 
 public class Player implements java.io.Serializable
 {
+    private String strUserName;
+    private short shrScore;
+    private short shrPersonalHighScore;
     
+    Player(String un, short s, short phs)
+    {
+        this.strUserName = un;
+        this.shrScore = s;
+        this.shrPersonalHighScore = phs;
+        
+    }
+    
+    Player()
+    {
+       this.strUserName = "unknown";
+       this.shrScore = 0;
+       this.shrPersonalHighScore = 0;
+        
+    }
 }
